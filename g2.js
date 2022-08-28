dbbb874c43b7a4e48977c01fb0452bdb3a2778d0
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 16:43:06 +0700

    update g2.js

--- g2.js
+++ g2.js
@@ -1,13 +1,17 @@
 let g2s = `
 august ames
+connie carter
+danielle delaunay
 harley dean
 lisa ann
+
 julia ann
 julie kay
+kagney linn karter
 melinda doll
 mia malkova
 naomi woods
 remy lacroix
 rita argilles
-siri
+siri dahl
 `;
